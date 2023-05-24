c4b7519dd32ebb235a0d09de4505798803e2ed6b
vxvishal <70997401+vxvishal@users.noreply.github.com>
2023-05-24 22:24:54 +0530

    Add files via upload

---
+++ Chatbot.js
@@ -0,0 +1,33 @@
+module.exports =
+{
+
+	ChatbotReply: function (message) {
+		// Bot's data
+		this.Bot_Age = 22;
+		this.Bot_Name = "Bittu";
+		this.Bot_University = "Jain University";
+		this.Bot_Country = "India";
+		// User data
+		this.User_Age;
+		this.User_Name;
+		this.User_University;
+		// Message processing... 
+		message = message.toLowerCase()
+
+		if (message.indexOf("hi") > -1 || message.indexOf("hello") > -1 || message.indexOf("welcome") > -1) {
+			return "Hi!";
+		}
+		else if (message.indexOf("age") > -1 && message.indexOf("your")) {
+			return "I'm " + this.Bot_Age;
+		}
+		else if (message.indexOf("how") > -1 && message.indexOf("are") && message.indexOf("you")) {
+			return "I'm fine ^_^"
+		}
+		else if (message.indexOf("where") > -1 && message.indexOf("live") && message.indexOf("you")) {
+			return "I live in " + this.Bot_Country;
+		}
+		return "Sorry, I didn't get it :( ";
+	}
+
+
+};
